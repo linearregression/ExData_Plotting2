8ff570bcec7fc638806a1cdd67e3fc057cab967e
root <edward>
2014-10-22 14:18:52 -0700

    plot3

--- plot3.R
+++ plot3.R
@@ -15,20 +15,20 @@ getdata <- function() {
 }
 
 plot1<-function(){
-    require(data.table)
+    #require(data.table)
     require(dplyr)
     require(ggplot2)
     png('plot3.png', 960, 960, bg="white")
     # Load the NEI & SCC data frames.
-    dataset <- as.data.table(readRDS("summarySCC_PM25.rds"), keep.rownames=F)
+    dataset <- readRDS("summarySCC_PM25.rds")
    
     # Sum of Emissions in Baltimore, Maryland(fips==24510) 
     # by year to Million Tons
     dataset <- subset(dataset, subset=(fips=='24510'), select=c(Emissions, type, year))
-    dataset <- aggregate(Emissions ~type + year, daa=dataset, sum, na.rm=TRUE)
-    g <- ggplot(data = dataset, mapping = aes(x=year, y = Emissions, fill=type)) +
-        layer(geom = 'histogram', geom_params = list(fill='green',color = 'steelblue'), stat = 'identity')
-        facet_grid(.~type, scales = "free", space="free") + 
+    dataset <- aggregate(Emissions ~type + year, data=dataset, sum, na.rm=TRUE)
+    g <- ggplot(data = dataset, mapping = aes(x=factor(year), y = Emissions, fill=type)) +
+        layer(geom = 'histogram', geom_params = list(color = 'steelblue'), stat = 'identity') +
+        facet_grid(. ~type, scales = "free", space="free") + 
         labs(x="Year",
              y= expression("Total PM"[2.5]*" Emission (Tons)"),
              title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type")) 
